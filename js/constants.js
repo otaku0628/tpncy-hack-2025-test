428903c8c52e77bb90bb3f5a7ef97a81c43c39f5
Shengfu Zhang <zhang.aptx4869@gmail.com>
2025-05-22 04:04:30 -0700

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,10 +1,19 @@
-// js/constants.js
-export const SCAN_INTERVAL = 100;
-export const BATCH_INTERVAL = 3000;
-export const MAX_ATTEMPTS = 3;
+export const DetectedCodeType = {
+    AZ_CODE: 'AZ_CODE',
+    SGTIN: 'SGTIN',
+    LGTIN: 'LGTIN',
+    GTIN: 'GTIN',
+    UNKNOWN: 'UNKNOWN'
+};
 
 export const DETECTOR_CONFIGS = {
-    dataMatrix: { formats: ["data_matrix"] },
-    qrCode: { formats: ["qr_code"] },
-    gtin: { formats: ["ean_13", "ean_8", "upc_a", "upc_e"] }
+    dataMatrix: {
+        formats: ["data_matrix"]
+    },
+    qrCode: {
+        formats: ["qr_code", "micro_qr_code", "rm_qr_code"]
+    },
+    gtin: {
+        formats: ["ean_13", "ean_8", "upc_a", "upc_e"]
+    }
 };
