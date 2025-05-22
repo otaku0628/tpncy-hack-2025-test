3fb47b97f1f78ec130f140e8192502c15f851139
Shengfu Zhang <zhang.aptx4869@gmail.com>
2025-05-21 23:38:18 -0700

    Create constants.js

---
+++ js/constants.js
@@ -0,0 +1,16 @@
+// js/constants.js
+export const SCAN_INTERVAL = 100;
+export const BATCH_INTERVAL = 3000;
+export const MAX_ATTEMPTS = 3;
+
+export const DETECTOR_CONFIGS = {
+    dataMatrix: { formats: ["data_matrix"] },
+    qrCode: { formats: ["qr_code"] },
+    gtin: { formats: ["ean_13", "ean_8", "upc_a", "upc_e"] },
+    others: {
+        formats: [
+            "codabar", "code_39", "code_93", "code_128",
+            "databar", "databar_limited", "databar_expanded"
+        ]
+    }
+};
